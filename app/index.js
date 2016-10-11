0f13889d7a4c11ed999c0a1c928cfef79bfaace1
Roger Lam <mrlamroger@gmail.com>
2016-10-11 14:14:12 -0700

    Lets get some nested components up in here!

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: 'Roger Lam',
+  username: 'mrlamroger',
+  imageUrl: 'https://avatars0.githubusercontent.com/u/601386?v=3&s=466'
+}
+
+var ProfilePic = React.createClass({
   render: function () {
     return (
-      <div>Hello React</div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    )
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return ( 
+      <div>
+        <ProfilePic imageUrl={this.props.user.imageUrl} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDom.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
