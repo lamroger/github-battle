adfd2668ef6a6bc3c548c41bc6e82dd9a1b29ed7
Roger Lam <mrlamroger@gmail.com>
2016-10-11 23:48:48 -0700

    Create main page and first and second player pages. Use react router, container components, and presentational components

--- app/index.js
+++ app/index.js
@@ -3,24 +3,22 @@ var ReactDom = require('react-dom');
 var routes = require('./config/routes');
 var Raven = require('raven-js');
 
-var sentryKey = 'supersecretkey'
-var sentryApp = 'supersecretappid'
-var sentryURL = 'https://' + sentryKey + '@sentry.io/' + sentryApp
+// var sentryKey = 'supersecretkey'
+// var sentryApp = 'supersecretappid'
+// var sentryURL = 'https://' + sentryKey + '@sentry.io/' + sentryApp
 
-var _APP_INFO = {
-  name: 'Github Battle',
-  branch: 'video4',
-  version: '1.0'
-}
+// var _APP_INFO = {
+//   name: 'Github Battle',
+//   branch: 'video4',
+//   version: '1.0'
+// }
 
-Raven.config(sentryURL, {
-  release: _APP_INFO.version,
-  tag: {
-    branch: _APP_INFO.branch,
-  }
-}).install()
-
-console.log(window.thing.doesNotExist);
+// Raven.config(sentryURL, {
+//   release: _APP_INFO.version,
+//   tag: {
+//     branch: _APP_INFO.branch,
+//   }
+// }).install()
 
 ReactDom.render(
   routes,
